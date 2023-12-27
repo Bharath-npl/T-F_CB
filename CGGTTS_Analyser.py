91bc66e387a3e2a0920ea8fa1c8a20b509c05521
Bharath-npl <98375674+Bharath-npl@users.noreply.github.com>
2023-12-27 15:20:31 +0100

    Update CGGTTS_Analyser.py

--- CGGTTS_Analyser.py
+++ CGGTTS_Analyser.py
@@ -79,9 +79,12 @@ def display_pdf_from_url(url):
         st.error("Failed to load PDF")
 
 if st.sidebar.checkbox('Time transfer through GNSS'):
+    st.markdown("[View Expert Presentation](https://raw.githubusercontent.com/Bharath-npl/T-F_CB/main/Expert_presentation.pdf)", unsafe_allow_html=True)
+
+# if st.sidebar.checkbox('Time transfer through GNSS'):
     # display_pdf_from_url('https://github.com/Bharath-npl/T-F_CB/blob/main/Expert_presentation.pdf')
     # display_pdf_from_url('https://github.com/Bharath-npl/T-F_CB/main/Expert_presentation.pdf')
-    display_pdf_from_url('https://raw.githubusercontent.com/Bharath-npl/T-F_CB/main/Expert_presentation.pdf')
+    # display_pdf_from_url('https://raw.githubusercontent.com/Bharath-npl/T-F_CB/main/Expert_presentation.pdf')
 # else:
 #     st.write("PDF is hidden. Check the box to view it.")
 
